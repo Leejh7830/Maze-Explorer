62e446019780ed07f434fbe90e1593097ec18003
Lee JaeHyeong <96864400+Leejh7830@users.noreply.github.com>
2024-05-14 08:56:59 +0900

    20240514 09:00 기본베이스 출력 완료

--- Maze Explorer/Board.cs
+++ Maze Explorer/Board.cs
@@ -8,5 +8,25 @@ namespace Maze_Explorer
 {
     class Board
     {
+        const char CIRCLE = '\u25cf';
+        public int _size;
+
+        public void InitializeBoard(int size)
+        {
+            _size = size;
+        }
+
+        public void Render()
+        {
+            for (int y = 0; y < _size; y++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green; // 초록미로
+
+                for (int x = 0; x < _size; x++)
+                    Console.Write(CIRCLE);
+
+                Console.WriteLine();
+            }
+        }
     }
 }
--- Maze Explorer/Program.cs
+++ Maze Explorer/Program.cs
@@ -10,7 +10,34 @@ namespace Maze_Explorer
     {
         static void Main(string[] args)
         {
+            const int WAIT_TICK = 1000 / 30;
 
+            Board board = new Board();
+            board.InitializeBoard(25);
+
+            Console.CursorVisible = false;
+
+            int lastTick = 0;
+            while (true)
+            {
+                #region 프레임 관리
+                int currentTick = System.Environment.TickCount;
+
+                //경과 시간이 1/30초보다 작다면?
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+
+                lastTick = currentTick;
+                #endregion
+
+                // 1)사용자 입력 대기
+
+                // 2)입력과 기타 로직 처리
+
+                // 3)렌더링
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
         }
     }
 }
